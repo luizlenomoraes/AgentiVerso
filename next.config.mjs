dfa8fecb07336c389bea6c7b7fd7a03ebf3b8e42
luizlenomoraes <grupovipiqoption@gmail.com>
2025-12-31 16:03:21 -0300

    mega atualização 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // Otimiza para Docker
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
